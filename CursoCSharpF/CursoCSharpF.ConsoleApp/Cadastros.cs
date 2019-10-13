264bac212ce3d4374039bcc3e09f3ac1638af734
Eduardo Caetano <eduardo.kaetano@gmail.com>
2019-10-13 18:54:34 -0300

    Calculadora

---
+++ CursoCSharpF/CursoCSharpF.ConsoleApp/Cadastros.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoCSharpF.ConsoleApp
+{
+    class Cadastros
+    {
+        public static string CadastroPessoa()
+        {
+            Console.WriteLine("========================Cadastro de Pessoa========================");
+            Console.Write("Escreva seu nome: ");
+            string primeiroNome = Console.ReadLine();
+
+            Console.Write("Escreva seu Sobrenome: ");
+            string sobrenome = Console.ReadLine();
+
+            Console.Write("Escreva sua idede: ");
+            int idade = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("==================================================================");
+
+            //========= Imprimindo com concatenação
+            //Console.WriteLine("O nome é: " + primeiroNome + " Sobrenome " + sobrenome + " Idade: " + idade);
+            //========= Imprimindo com interpolação de strings
+            //Console.WriteLine($"O nome é: {primeiroNome} Sobrenome {sobrenome} Idade: {idade}");
+            string pessoa = $"O nome é: {primeiroNome} Sobrenome {sobrenome} Idade: {idade}";
+            return pessoa;
+        }
+    }
+}
--- CursoCSharpF/CursoCSharpF.ConsoleApp/Program.cs
+++ CursoCSharpF/CursoCSharpF.ConsoleApp/Program.cs
@@ -10,25 +10,64 @@ namespace CursoCSharpF.ConsoleApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("========================Cadastro de Pessoa========================");
+            //string nome = Cadastros.CadastroPessoa();
+            //Console.WriteLine( nome);
 
-            Console.Write("Escreva seu nome: ");
-            string primeiroNome = Console.ReadLine();
+            //int idade = RetornaIdade();
+            //Console.WriteLine(idade);
+            //Console.ReadLine();
+            Console.Write("Digite um Valor: ");
+            int n1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Digite um outro Valor: ");
+            int n2 = Convert.ToInt32(Console.ReadLine());
 
-            Console.Write("Escreva seu Sobrenome: ");
-            string sobrenome = Console.ReadLine();
+            Console.WriteLine("________________________________________________");
 
-            Console.Write("Escreva sua idede: ");
-            int idade = Convert.ToInt32(Console.ReadLine());
+            int numero = Soma(n1, n2);
+            Console.WriteLine($"A soma é {numero}");
 
-            Console.WriteLine("==================================================================");
+            numero = Divi(n1, n2);
+            Console.WriteLine($"A divisão é {numero}");
+           
+            numero = Mult(n1, n2);
+            Console.WriteLine($"A Multiplicação é {numero}");
+            
 
-            //========= Imprimindo com concatenação
-            Console.WriteLine("O nome é: " + primeiroNome + " Sobrenome " + sobrenome + " Idade: " + idade);
-            //========= Imprimindo com interpolação de strings
-            Console.WriteLine($"O nome é: {primeiroNome} Sobrenome {sobrenome} Idade: {idade}");
+            numero = Modulo(n1, n2);
+            Console.WriteLine($"A sovbra da divisão é {numero}");
+
 
             Console.ReadLine();
+        }
+
+        static int Soma(int numero1, int numero2)
+        {
+
+            int soma = numero1 + numero2;
+
+            return soma;
+
+        }
+        static int Divi(int numero1, int numero2)
+        {
+
+            int divi = numero1 / numero2;
+
+            return divi;
+        }
+        static int Mult(int numero1, int numero2)
+        {
+
+            int mult = numero1 * numero2;
+
+            return mult;
+        }
+        static int Modulo(int numero1, int numero2)
+        {
+
+            int modulo = numero1 % numero2;
+
+            return modulo;
         }
     }
 }
